bc754bfe3c9f94906484a69a075cab5a8572ae60
Soha Ahmed Hamdy <soha7hmed@gmail.com>
2023-07-24 16:15:37 +0300

    Initial Commit

---
+++ Superhero/SuperheroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperheroApp.swift
+//  Superhero
+//
+//  Created by Soha Ahmed Hamdy on 24/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SuperheroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
